bfe72994c237ff60b58b1c7ba11181365103e8ae
DevonMcfarlen <mcfarlendevon@gmail.com>
2023-12-03 20:55:27 -0600

    fixed vite install and minor updates to server

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  server: {
     port: 3000,
     open: true,
     proxy: {
@@ -12,7 +12,7 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
         secure: false,
-      },
+      }
     }
   }
 })
