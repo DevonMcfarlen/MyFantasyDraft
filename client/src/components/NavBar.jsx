9b34a628fc801357393f260748da81ba2fa6dcff
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-04 15:38:15 -0600

    changed css filename

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,19 @@
+import '../../style/NavBar.css';
+
 const NavBar = () => {
 
   return (
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Home</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link" href="/cards">Players</a>
-                <a class="nav-item nav-link" href="/profile">Profile</a>
-            </div>
+        <div className="container"></div>
+            <a class="navbar-brand" href="/">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link" href="/cards">Players</a>
+                    <a class="nav-item nav-link" href="/profile">Profile</a>
+                </div>
         </div>
     </nav>
   );
