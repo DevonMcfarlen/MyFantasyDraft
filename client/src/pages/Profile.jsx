ce5a9499df3a03153be131bdf0297bcdd0658507
jae kim <jaehkim23@gmail.com>
2023-12-05 14:38:48 -0600

    edited profile page/added trash icons

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,15 @@
 import '../../style/Profile.css'
+import { FaRegTrashAlt } from "react-icons/fa";
 
 const Profile = () =>{
     return(
         <table className="table table-striped">
             <thead>
             <tr>
-                <th scope="col">Jersey #</th>
+                <th scope="col" className='jerseyNum'>Jersey #</th>
                 <th scope="col">First</th>
                 <th scope="col">Last</th>
+                <th scope='col'>Remove</th>
             </tr>
             </thead>
             <tbody>
@@ -15,51 +17,61 @@ const Profile = () =>{
                 <th scope="row">1</th>
                 <td className='firstName'>Mark</td>
                 <td className='lastName'>Otto</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">2</th>
                 <td className='firstName'>Jacob</td>
                 <td className='lastName'>Thornton</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">3</th>
                 <td className='firstName'>Larry</td>
                 <td className='lastName'>the Bird</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">4</th>
                 <td className='firstName'>Mark</td>
                 <td className='lastName'>Otto</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">5</th>
                 <td className='firstName'>Jacob</td>
                 <td className='lastName'>Thornton</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">6</th>
                 <td className='firstName'>Larry</td>
                 <td className='lastName'>the Bird</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">7</th>
                 <td className='firstName'>Mark</td>
                 <td className='lastName'>Otto</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">8</th>
                 <td className='firstName'>Jacob</td>
                 <td className='lastName'>Thornton</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">9</th>
                 <td className='firstName'>Larry</td>
                 <td className='lastName'>the Bird</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             <tr>
                 <th scope="row">10</th>
                 <td className='firstName'>Larry</td>
                 <td className='lastName'>the Bird</td>
+                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
             </tbody>
       </table>
