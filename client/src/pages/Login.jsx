37f451b7abb2146cbb03afbd17251ba5e9a0b481
jae kim <jaehkim23@gmail.com>
2023-12-06 11:53:42 -0600

    fixed login merge

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,6 +5,8 @@ import {useMutation} from '@apollo/client';
 import {LOGIN_USER} from '../utils/mutations';
 import { ADD_USER } from '../utils/mutations';
 import auth from '../utils/auth';
+import { useState } from 'react';
+
 const Login = () => {
     const [formState, setFormState] = useState({ email: '', password: '' });
     const [login, { error, data }] = useMutation(LOGIN_USER);
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import '../../style/Profile.css'
 import { FaRegTrashAlt } from "react-icons/fa";
 
-const Profile = () =>{
+const Profile = () =>{ 
     return(
         <table className="table table-striped">
             <thead>
@@ -19,62 +19,67 @@ const Profile = () =>{
                 <td className='lastName'>Otto</td>
                 <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
             </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td className='firstName'>Jacob</td>
-                <td className='lastName'>Thornton</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td className='firstName'>Larry</td>
-                <td className='lastName'>the Bird</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">4</th>
-                <td className='firstName'>Mark</td>
-                <td className='lastName'>Otto</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">5</th>
-                <td className='firstName'>Jacob</td>
-                <td className='lastName'>Thornton</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">6</th>
-                <td className='firstName'>Larry</td>
-                <td className='lastName'>the Bird</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">7</th>
-                <td className='firstName'>Mark</td>
-                <td className='lastName'>Otto</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">8</th>
-                <td className='firstName'>Jacob</td>
-                <td className='lastName'>Thornton</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">9</th>
-                <td className='firstName'>Larry</td>
-                <td className='lastName'>the Bird</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            <tr>
-                <th scope="row">10</th>
-                <td className='firstName'>Larry</td>
-                <td className='lastName'>the Bird</td>
-                <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
-            </tr>
-            </tbody>
+            </tbody> 
       </table>
+
+
+
+    //          <tr>
+    //             <th scope="row">2</th>
+    //             <td className='firstName'>Jacob</td>
+    //             <td className='lastName'>Thornton</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">3</th>
+    //             <td className='firstName'>Larry</td>
+    //             <td className='lastName'>the Bird</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">4</th>
+    //             <td className='firstName'>Mark</td>
+    //             <td className='lastName'>Otto</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">5</th>
+    //             <td className='firstName'>Jacob</td>
+    //             <td className='lastName'>Thornton</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">6</th>
+    //             <td className='firstName'>Larry</td>
+    //             <td className='lastName'>the Bird</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">7</th>
+    //             <td className='firstName'>Mark</td>
+    //             <td className='lastName'>Otto</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">8</th>
+    //             <td className='firstName'>Jacob</td>
+    //             <td className='lastName'>Thornton</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">9</th>
+    //             <td className='firstName'>Larry</td>
+    //             <td className='lastName'>the Bird</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         <tr>
+    //             <th scope="row">10</th>
+    //             <td className='firstName'>Larry</td>
+    //             <td className='lastName'>the Bird</td>
+    //             <td><button className='trashBtn'><FaRegTrashAlt/></button></td>
+    //         </tr>
+    //         </tbody> 
+    //   </table>
     )
 }
 export default Profile 
