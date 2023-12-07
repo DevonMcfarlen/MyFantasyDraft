3d7b2e4a4189af81e7687f9a5abbd436ad89d934
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-07 11:24:24 -0600

    Fixing type input

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
             variables: { ...formState },
           });
     
-          Auth.login(data.addUser.token);
+          auth.login(data.addUser.token);
         } catch (e) {
           console.error(e);
         }
@@ -92,32 +92,3 @@ const Login = () => {
   }
 
 export default Login;
-
-
-
-/*
-
-//  <div className="login-container">
-// /* Login Card */
-// <div className="card login-card">
-//     <h2>Login</h2>
-//     <form>
-//         <input type="Email" placeholder="Email" />
-//         <input type="text" placeholder="Username" />
-//         <input type="password" placeholder="Password" />
-//         <button type="submit">Login</button>
-//     </form>
-// </div> 
-
-// {/* Signup Card */}
-//  <div className="card signup-card">
-//     <h2>Sign Up</h2>
-//     <form>
-//         <input type="email" placeholder="Email" />
-//         <input type="text" placeholder="Username" />
-//         <input type="password" placeholder="Password" />
-//         <button type="submit">Sign Up</button>
-//     </form>
-// </div>
-// </div> 
-
