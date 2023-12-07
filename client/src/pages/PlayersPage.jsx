8d82fe70ec27fa3819774018e89a2f4ad9993b1e
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-06 18:11:55 -0600

    team name is now showing instead of ID name

--- client/src/pages/PlayersPage.jsx
+++ client/src/pages/PlayersPage.jsx
@@ -40,7 +40,6 @@ function PlayersPage() {
     ])
 
 
-
     const bingSettings = {
         async: true,
         crossDomain: true,
@@ -88,11 +87,10 @@ function PlayersPage() {
         $.ajax(bingSettings).done(function (response) {});
     };
 
-
     return (
         <>
             <header>
-                <h1>{selectedTeam ? `${selectedTeam}'s Player Statistics` : 'NBA Player Statistics'}</h1>
+                <h1>{selectedTeam ? `${teams.find((team) => team.id === Number(selectedTeam)).name}'s Player Statistics` : 'NBA Player Statistics'}</h1>
             </header>
             <div>
                 <select value={selectedTeam} onChange={(e) => setSelectedTeam(e.target.value)}>
@@ -112,137 +110,4 @@ function PlayersPage() {
     )
 }
 
-
 export default PlayersPage;
-
-/*
-    return (
-        <>
-            <header>
-                <h1>{selectedTeam ? `${selectedTeam}'s Player Statistics` : "NBA Player Statistics"}</h1>
-            </header>
-            <div>
-                <select
-                    value={selectedTeam}
-                    onChange={(e) => setSelectedTeam(e.target.value)}
-                >
-                    <option value="">Select a team</option>
-                    <option value="8">Dallas Mavericks</option>
-                    <option value="5">Charlotte Hornets</option>
-                </select>
-                <button
-                    type="button"
-                    onClick={getTeam}
-                >
-                    Search
-                </button>
-            </div>
-            {teamPlayers.length > 0 && (
-                <Cards 
-                    teamPlayers={teamPlayers}
-                />
-            )}
-        </>
-    );
-};
-
-export default PlayersPage;
-
-
-
-/*
-import { useState } from 'react';
-import Cards from './Cards';
-
-import '../../style/Cards.css'
-import $ from "jquery";
-
-function PlayersPage() {
-    const [teamPlayers, setTeamPlayers] = useState([]);
-    const [selectedTeam, setSelectedTeam] = useState('');
-
-    var bingSettings = {};
-
-    function setBingSettings(sentUrl) {
-        var bingUrl = "https://bing-image-search1.p.rapidapi.com/images/search?q=" + sentUrl;
-        bingSettings = {
-            async: true,
-            crossDomain: true,
-            url: bingUrl,
-            method: 'GET',
-            headers: {
-            'X-RapidAPI-Key': '23621a63e4msha9fc92986a51b9fp1bfe54jsn18d7163659ca',
-            'X-RapidAPI-Host': 'bing-image-search1.p.rapidapi.com'
-            },
-        };
-    }
-
-    var userSeason = 2023;
-
-    var nbaSettings = {};
-
-    function setNBASettings(sentUrl) {
-        var nbaUrl = "https://api-nba-v1.p.rapidapi.com" + sentUrl;
-        nbaSettings = {
-            "url": nbaUrl,
-            "method": "GET",
-            "timeout": 0,
-            "headers": {
-            "x-rapidapi-key": "09e77c0237msh7cf79c6d0985d69p119f9cjsnb5866ab39fe8",
-            "x-rapidapi-host": "api-nba-v1.p.rapidapi.com"
-            },
-        };
-    }
-
-    function getTeam() {
-        let toSendUrl = "/players/?team=8&season=" + userSeason;
-        setNBASettings(toSendUrl);
-
-        return $.ajax(nbaSettings).done(function (response) {
-            setTeamPlayers(response.response);
-        });
-    }
-
-    function getPlayerImage(player) {
-        let toSendUrl = 'professional+headshot+of+' + player.firstname + '+' + player.lastname + '+from+espn.com+bio';
-        setBingSettings(toSendUrl);
-
-        return $.ajax(bingSettings).done(function (response) {});
-    }
-
-    return teamPlayers.length == 0 ? (
-        <>
-            <header>
-                <h1>NBA Player Statistics</h1>
-            </header>
-            <button
-                type="button"
-                onClick={() => {
-                    getTeam();
-                }}
-                >
-                Search
-            </button>
-        </>
-    ) : (
-        <>
-            <header>
-                <h1>Dallas Maverick's Player Statistics</h1>
-            </header>
-            <button
-                type="button"
-                onClick={() => {
-                    getTeam();
-                }}
-                >
-                Search
-            </button>
-            <Cards 
-                teamPlayers={teamPlayers}
-            />
-        </>
-    );
-};
-
-export default PlayersPage 
-*/
