cd1b53d0b2643647b9178e661e240c39db7ef08f
prayushapandey04 <prayusha.pandey4@gmail.com>
2023-12-07 21:18:05 -0600

    routing users to players page after logging in and then routing to home page after logging out

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -24,7 +24,7 @@ class AuthService {
 
     login(idToken) {
         localStorage.setItem('id_token', idToken);
-        window.location.assign('/');
+        window.location.assign('/players');
     }
 
     logout() {
