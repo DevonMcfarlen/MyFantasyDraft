d39b203dc6803bb0fa54a88e5760c24f21f08585
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-09 14:13:55 -0600

    remove player functionality (wip)

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,6 +3,8 @@ import '../../style/Profile.css'
 import { FaRegTrashAlt } from "react-icons/fa";
 import NavBar from '../components/NavBar';
 import { useQuery, gql } from '@apollo/client'
+import { useMutation } from '@apollo/client';
+import { REMOVE_PLAYER } from '../utils/mutations';
 // import { useState } from 'react';
 
 
@@ -26,6 +28,19 @@ const Profile = () =>{
     variables: { username: localStorage.getItem('username') },
   });
 
+  const [removePlayer] = useMutation(REMOVE_PLAYER);
+
+  const handleRemovePlayer = async (playerId) => {
+    try {
+      const { data } = await removePlayer({
+        variables: { playerId },
+      });
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error: {error.message}</p>;
 
@@ -48,16 +63,20 @@ const Profile = () =>{
             </thead>
             <tbody>
               {!data.user.players.length ? ('') : (data.user.players.map((player) => (
-                <tr key={player._id}>
-                <th scope="row" className='bg-transparent jerseyNum' style={{color:'white',fontFamily:'Montserrat' }}>{player.jersey}</th>
-                <td className='bg-transparent name' style={{color:'white',fontFamily:'Montserrat'}}>{player.name}</td>
-                <td className='bg-transparent stats' style={{color:'white',fontFamily:'Montserrat'}}>{player.stats}</td>
-                <td className='bg-transparent'><button className='trashBtn' style={{color:'white'}} ><FaRegTrashAlt/></button></td>
-            </tr>
-            )))}
-            </tbody> 
-        </table>
-        </div>
-    )
-}
+                 <tr key={player._id}>
+                 <th scope="row" className='bg-transparent jerseyNum' style={{color:'white',fontFamily:'Montserrat' }}>{player.jersey}</th>
+                 <td className='bg-transparent name'style={{color:'white',fontFamily:'Montserrat'}}>{player.name}</td>
+                 <td className='bg-transparent stats'style={{color:'white',fontFamily:'Montserrat'}}>{player.stats}</td>
+                 <td className='bg-transparent'>
+                   <button className='trashBtn' style={{ color: 'white' }} onClick={() => handleRemovePlayer(player._id)}>
+                     <FaRegTrashAlt />
+                   </button>
+                 </td>
+               </tr>
+             )))}
+           </tbody>
+         </table>
+       </div>
+     )
+   }
 export default Profile 
